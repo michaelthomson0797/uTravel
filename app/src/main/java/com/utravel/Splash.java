deeffa66bd7553a4d65fb4ebef7cea309ad8b039
Michael Thomson <michaelthomson0797@gmail.com>
2018-04-08 23:57:29 -0400

    permissions are now requested on startup

--- app/src/main/java/com/utravel/Splash.java
+++ app/src/main/java/com/utravel/Splash.java
@@ -1,10 +1,13 @@
 package com.utravel;
 
+import android.*;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -21,6 +24,8 @@ public class Splash extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        isStoragePermissionGranted();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -29,6 +34,27 @@ public class Splash extends AppCompatActivity {
                 Splash.this.finish();
             }
         }, SPLASH_DELAY);
+    }
+
+    //checks if storage permission is granted
+    public boolean isStoragePermissionGranted() {
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                            1);
+
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            }
+            return false;
+        }
     }
 
 }
