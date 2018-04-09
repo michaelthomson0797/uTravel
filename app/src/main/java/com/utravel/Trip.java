8e3b5084f4b8c20b7ca51679c327696ecbecfcbe
Michael Thomson <michaelthomson0797@gmail.com>
2018-04-08 20:18:19 -0400

    Created a Trip class to hold trip info

---
+++ app/src/main/java/com/utravel/Trip.java
@@ -0,0 +1,67 @@
+package com.utravel;
+
+
+public class Trip {
+    //values for the flight
+    private String flightCode;
+    private String flightTimeCode;
+    private String flightAirline;
+    private String flightLayovers;
+    private String flightPrice;
+
+    //values for hotel
+    private String hotelName;
+    private String hotelScore;
+    private String hotelPrice;
+    private String hotelAddress;
+
+    public Trip(String flightCode, String flightTimeCode, String flightAirline,
+                String flightLayovers, String flightPrice, String hotelName, String hotelScore,
+                String hotelPrice, String hotelAddress) {
+        this.flightCode = flightCode;
+        this.flightTimeCode = flightTimeCode;
+        this.flightAirline = flightAirline;
+        this.flightLayovers = flightLayovers;
+        this.flightPrice = flightPrice;
+        this.hotelName = hotelName;
+        this.hotelScore = hotelScore;
+        this.hotelPrice = hotelPrice;
+        this.hotelAddress = hotelAddress;
+    }
+
+    public String getFlightCode() {
+        return flightCode;
+    }
+
+    public String getFlightTimeCode() {
+        return flightTimeCode;
+    }
+
+    public String getFlightAirline() {
+        return flightAirline;
+    }
+
+    public String getFlightLayovers() {
+        return flightLayovers;
+    }
+
+    public String getFlightPrice() {
+        return flightPrice;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public String getHotelScore() {
+        return hotelScore;
+    }
+
+    public String getHotelPrice() {
+        return hotelPrice;
+    }
+
+    public String getHotelAddress() {
+        return hotelAddress;
+    }
+}
